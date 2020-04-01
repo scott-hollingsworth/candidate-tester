b5d02b3f9da7a0ec962f108e743454da4a577a0f
Scott Hollingsworth <scott.hollingsworth@wipro.com>
2020-04-01 09:14:30 -0500

    scripts saved

--- check-git-commits.sh
+++ check-git-commits.sh
@@ -4,6 +4,6 @@ set -o pipefail
 #check if get commit count is greater than 2
 output = git rev-list --all --count
 if ! (($output > 2)) then
-    echo "Git commit count is not greater than 2. Count = $output"
+    echo "Git commit count is not greater than 2. Count = $output."
     EXIT 1
 fi
