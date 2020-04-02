bcf4be79e4080b04dfb4f162bcaf75ffda2d3dc1
Chris Ashurst <chris@telestatic.net>
2020-04-02 12:49:49 -0400

    updated javascript-webcrawler codebase and build steps

--- javascript-webcrawler/e2e/jest.config.js
+++ javascript-webcrawler/e2e/jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   preset: 'jest-puppeteer',
   testRegex: './*\\.test\\.js$',
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: 'test-reports',
+      outputName: 'e2e.junit.xml',
+    }],
+  ],
 }
