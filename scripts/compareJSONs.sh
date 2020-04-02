a3669c58427e4694d58e44c8fd3c7790617020c1
Scott Hollingsworth <scott.hollingsworth@wipro.com>
2020-04-02 15:06:52 -0500

    output test script

--- scripts/compareJSONs.sh
+++ scripts/compareJSONs.sh
@@ -2,9 +2,12 @@ set -eu
 set -o pipefail
 
 #sort json entries and compare
-output = $(jq --argfile expected $0 --argfile actual $1 -n 'def post_recurse(f): def r: (f | select(. != null) | r), .; r; def post_recurse: post_recurse(.[]?); ($expected | (post_recurse | arrays) |= sort) as $expected | ($actual | (post_recurse | arrays) |= sort) as $actual | $expected == $actual')
+output=$(jq --argfile expected $1 --argfile actual $2 -n 'def post_recurse(f): def r: (f | select(. != null) | r), .; r; def post_recurse: post_recurse(.[]?); ($expected | (post_recurse | arrays) |= sort) as $expected | ($actual | (post_recurse | arrays) |= sort) as $actual | $expected == $actual')
 
-if ! (($output)) then
-    echo "Expected and Actual output do not match"
-    EXIT 1
+if ! ($output); then
+    echo Expected and Actual output \\do not match
+    echo !!! Output Check FAILED !!!
+    exit 1
 fi
+
+echo --- Output Check Passed ---
