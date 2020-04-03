71bdae39133cbbd7f01a078d18921e41d2db6b6f
Scott Hollingsworth <scott.hollingsworth@wipro.com>
2020-04-02 20:05:37 -0500

    detect if readme exists

--- scripts/compare-README.sh
+++ scripts/compare-README.sh
@@ -2,8 +2,17 @@
 originalREADME="$1/README.md"
 candidateREADME="$2/README.md"
 #should pull original readme from starter repo in the future in case of changes
+
+#Check if README exists
+if [ -f "$candidateREADME" ]; then
+    echo !!! README Check Failed - README DOES NOT EXISTS !!!
+    echo Ensure it is \in the right folder    
+    exit 1
+fi
+#Compare README
 if cmp "$originalREADME" "$candidateREADME"; then
     echo !!! README Check Failed - No Change Detected !!!
+    exit 1
 else
     echo --- README Check Passed ---
 fi
