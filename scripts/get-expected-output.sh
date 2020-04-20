2946b4e2b6b91a202263d798d076edafc830028a
Scott Hollingsworth <scott.hollingsworth@wipro.com>
2020-04-20 16:08:52 -0500

    script fixes

--- scripts/get-expected-output.sh
+++ scripts/get-expected-output.sh
@@ -2,6 +2,13 @@ echo Getting Expected Output...
 ./expected-solution/gradlew clean bootrun &> /dev/null &
 sleep 30
 
+if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null ; then
+    echo ERROR Obtaining expected.json
+    echo !!! Pipeline Failure !!!
+    echo Rerun the Pipeline 
+fi
+
 curl http://localhost:8080/crawl?url=$TEST_DOMAIN -o expected.json
+kill -9 $(lsof -t -i:8080)
 
 echo expected.json Generated
